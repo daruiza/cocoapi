b98b30bde1cc7e3d3a48ba4a789bfd83b72ddca5
daruiza <daruiza@gmail.com>
2019-12-19 16:10:51 -0500

    up estilos para cocoluapi full organizados

--- src/app/modules/header/toolbarr/toolbarr.component.ts
+++ src/app/modules/header/toolbarr/toolbarr.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-toolbarr',
   templateUrl: './toolbarr.component.html',
-  styleUrls: ['./toolbarr.component.scss']
+  styleUrls: ['../../../../assets/css/header_toolbarr.css']
 })
 export class ToolbarrComponent implements OnInit {
+
+  @Input() title: string;
 
   constructor() { }
 
