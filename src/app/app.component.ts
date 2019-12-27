38c466390c4dc6d9b20deb69bab6a24bae76a90a
daruiza <daruiza@gmail.com>
2019-12-27 17:37:40 -0500

    menu lateral top menu dont work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['../assets/css/app.css']
 })
 export class AppComponent implements OnInit {
-  title = 'cocolú';
-  heigth: number;
+  title = 'Cocolú';
+  sidenavMode: string;
+  toolbarHeight = 60; // 60 es una bariable de configuracion de sass: $toolbar-height
+  windowHeight: number;
+  windowWidth: number;
+  iconSindenavWidth = 32; // tamaño de iconos de menu lateral
+
+
   ngOnInit(): void {
-    this.heigth = window.innerHeight;
+    // Control de tamaño, responsive
+    this.responsiveControl();
+  }
+
+  responsiveControl() {
+    // Width
+    this.windowWidth = window.innerWidth;
+    this.sidenavMode = this.windowWidth < 720 ? 'over' : 'side';
+
+
+    // Height
+    this.windowHeight = window.innerHeight - this.toolbarHeight;
   }
 
 
