3a87395608005c44f6bb0b3b9b3847ab84f33d59
daruiza <daruiza@gmail.com>
2020-01-12 20:57:19 -0500

    logout en proceso

--- src/app/modules/acces/logout/logout.component.ts
+++ src/app/modules/acces/logout/logout.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ModalAlertService } from 'src/app/services/modal-alert/modal-alert.service';
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['../../../../assets/css/acces_logout.css']
 })
-export class LogoutComponent implements OnInit {
+export class LogoutComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(
+    private readonly messagesAlertService: ModalAlertService
+  ) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.openModaLogout();
+  }
+
+  openModaLogout() {
+    this.messagesAlertService.openAlert({
+      title: 'Un Titulo',
+      type: 'success',
+      text: 'Alerta de de Suceso OK'
+    }).result.then((result) => {
+      // Consumo de servicio en caso de estar el form OK
+      console.log('result');
+    }, (reason) => {
+      console.log('reason');
+    });
   }
 
 }
